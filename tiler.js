815045bf276c1fa70d8a10c68b83dab3f9105a40
Florian Dejonckheere <florian@floriandejonckheere.be>
2014-08-18 11:06:12 +0200

    Added data elements

--- tiler.js
+++ tiler.js
@@ -6,18 +6,41 @@
  * */
 
 var palettes = ['colors03', 'platinum'];
-var colors = [];
+var current = 1;
+var tiles = [{
+	"title":		"Ideas",
+	"content":	"This is a tile for ideas"
+},{
+	"title":		"Notes",
+	"content":	"Write your notes here"
+},{
+	"title":		"Todo",
+	"content":	"Things to do before completing this list"
+},{
+	"title":		"Dump",
+	"content":	"Dump your brain's content here"
+}];
 
 $(document).ready(function(){
+	/* Load color palette */
 	var loadPalette = function(name){
 		$.getJSON('colors/' + name + '.json', function(data){
-				colors = data.colors;
-				// Assign colors
-				$.each(colors, function(index, value){
-					$('.color-' + index).css('background-color',	colors[index].bgColor);
-					$('.color-' + index).css('color',		(colors[index].textColor ? colors[index].textColor : '#FFFFFF'));
-				});
+			// Assign colors
+			$.each(data.tiles, function(index, value){
+				$('.color-' + index).css('background-color',	data.tiles[index].bgColor);
+				$('.color-' + index).css('color',		(data.tiles[index].textColor ? data.tiles[index].textColor : '#FFFFFF'));
 			});
+			$('.bg-color').css('background-color',	data.bgColor);
+			$('.text-color').css('color',		data.textColor);
+		});
+	}
+
+	var init = function(){
+		loadPalette(palettes[current]);
+		$.each(tiles, function(index, value){
+			console.log('Adding ' + value);
+			$('.wrapper tr').append('<td class="tile animated tile-inactive color-' + index + '"><span class="tile-title">' + value.title + '</span></td>');
+		});
 	}
 
 	/* MIME type hack */
@@ -28,13 +51,22 @@ $(document).ready(function(){
 	});
 
 	/* Load color palettes */
-	//$.each(palettes, function(index, value){
-		//loadPalette(value);
-		loadPalette(palettes[1]);
-	//});
-	
+	loadPalette(palettes[current]);
+
+	$('.wrapper tr td').last().hover(function(){
+		$('.tile-add').css('right', 0);
+	}, function(){
+		$('.tile-add').css('right', '-10%');
+	});
+
 	$('.tile').click(function(){
 		$('.tile').removeClass('tile-active').addClass('tile-inactive');
 		$(this).removeClass('tile-inactive').addClass('tile-active');
 	});
+
+	Mousetrap.bind('c',	function(){
+		loadPalette(palettes[(++current % palettes.length)]);
+	});
+
+	init();
 });
